b99e6c5e41966bd364add537fb0252b2a6abcf0a
Kairavparikh <kairav.parikh01@gmail.com>
2024-11-26 20:38:20 -0500

    Added lambda expression with method for addingt

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 public class Main {
+    
     @FunctionalInterface
     interface Number {
         void printNum(Integer answer);
@@ -12,10 +13,26 @@ public class Main {
         }
     }
 
+    @FunctionalInterface
+    interface addingLamdaExpression {
+        int addInBetweenNums(int start, int end);  
+    }
+
     public static void main(String[] args) {
         Number number = (answer) -> System.out.println("The sum between the min and the max is " + answer);
 
+        addingLamdaExpression adds = (start, end) -> {
+            int sum = 0;
+            for (int i = start; i <= end; i++) {
+                sum += i;
+            }
+            return sum;
+        };
+
         int result = Number.resultMethod(2, 5);
+        int result2 = adds.addInBetweenNums(2, 6);
         number.printNum(result);
+        number.printNum(result2);
+
     }
 }
